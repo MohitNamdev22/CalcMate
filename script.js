5d2959891cd2cf8061dae03ae96b352bfbaede4a
Piyush <piyush.motwani101@gmail.com>
2023-10-15 15:17:22 +0530

    Updated functionality and CSS

--- script.js
+++ script.js
@@ -25,3 +25,16 @@ function calculateResult() {
     document.getElementById('display').value = 'Error';
   }
 }
+
+// Mode shift
+let shiftOn = 0;
+function shiftMode() {
+  if (shiftOn == 0){
+    document.querySelector('.buttons2').style.display = "grid";
+    document.querySelector('.buttons').style.display = "none";
+    shiftOn = 1;
+  }else{
+    document.querySelector('.buttons2').style.display = "none";
+    document.querySelector('.buttons').style.display = "grid";
+    shiftOn = 0;}
+  }
