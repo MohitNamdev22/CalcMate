4654158573ce1bb4e100daf82b87b9ec21d2ec76
Amit Singh <iamitsinghthakur123@gmail.com>
2023-10-15 16:33:49 +0530

    Addition of more functionality Scientific Calc some styling is dis aligned but can be corrected in next commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let displayValue = '';
+let scientificMode = false;
 
 function appendToDisplay(value) {
   displayValue += value;
@@ -24,5 +25,59 @@ function calculateResult() {
     document.getElementById('display').value = displayValue;
   } catch (error) {
     document.getElementById('display').value = 'Error';
+  }
+}
+
+function log(){
+  displayValue = Math.log(displayValue);
+  document.getElementById('display').value = displayValue;
+}
+
+function log2(){
+  displayValue = Math.log2(displayValue);
+  document.getElementById('display').value = displayValue;
+}
+
+function log10(){
+  displayValue = Math.log10(displayValue);
+  document.getElementById('display').value = displayValue;
+}
+
+function sin(){
+  let radians = displayValue * Math.PI/180
+  displayValue = Math.sin(radians);
+  document.getElementById('display').value = displayValue;
+}
+
+function cos(){
+  let radians = displayValue * Math.PI/180
+  displayValue = Math.cos(radians);
+  document.getElementById('display').value = displayValue;
+}
+
+function tan(){
+  let radians = displayValue * Math.PI/180
+  displayValue = Math.tan(radians);
+  document.getElementById('display').value = displayValue;
+}
+
+function squareRoot(){
+  displayValue = Math.sqrt(displayValue);
+  document.getElementById('display').value = displayValue;
+}
+
+function toggleScientificMode(){
+  if(scientificMode){
+    document.getElementById('more').style.display = 'grid';
+    document.getElementById("calculator").style.width = "80vw";
+    document.getElementById("buttons").style.gridTemplateColumns = "repeat(3, 1fr)";
+    document.getElementById("icon").classList.replace("fa-flask", "fa-xmark");
+    scientificMode = false;
+  } else {
+    document.getElementById('more').style.display = 'none';
+    document.getElementById("calculator").style.width = "auto";
+    document.getElementById("buttons").style.gridTemplateColumns = "repeat(2, 1fr)";
+    document.getElementById("icon").classList.replace("fa-xmark", "fa-flask");
+    scientificMode = true;
   }
 }
